16cd1da5b45c47e1401b130e50f73fee222c7b23
Wesley Moy <wesleym@lambdal.com>
2025-01-02 14:39:05 -0500

    Initial commit

---
+++ bin/lambda_cli.dart
@@ -0,0 +1,5 @@
+import 'package:lambda_cli/lambda_cli.dart' as lambda_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lambda_cli.calculate()}!');
+}
---
+++ test/lambda_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:lambda_cli/lambda_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
