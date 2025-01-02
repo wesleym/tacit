ac944c35aa7f41ac67d1497743a424f4c0ea04ac
Wesley Moy <wesleym@lambdal.com>
2025-01-02 16:24:00 -0500

    Print instances as table

--- lib/instances.dart
+++ lib/instances.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
+import 'package:cli_table/cli_table.dart';
 import 'package:openapi/api.dart';
 
 class InstancesCommand extends Command<void> {
@@ -13,11 +14,31 @@ class InstancesCommand extends Command<void> {
 
   @override
   Future<void> run() async {
-    // TODO: Error handling.
-    final instances = await DefaultApi(defaultApiClient).listInstances();
+    final instancesFuture = DefaultApi(defaultApiClient).listInstances();
 
-    for (final instance in instances!.data) {
-      stdout.writeln(instance);
-    }
+    final table = Table(
+      header: [
+        'Name',
+        'Type',
+        'IP Address',
+        'Region',
+        'SSH Key',
+        'Status',
+      ],
+    );
+
+    // TODO: Error handling.
+    final instances = await instancesFuture;
+    final rows = instances!.data.map((i) => [
+          i.name,
+          i.instanceType!.name,
+          i.ip,
+          i.region!.name,
+          i.sshKeyNames.join(', '),
+          // TODO: Replace with correct user-facing name.
+          i.status.value,
+        ]);
+    table.addAll(rows);
+    stdout.writeln(table);
   }
 }
