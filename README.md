dc3d52df833f39f12a8055634d85cd322a4f3c54
Wesley Moy <wesleym@lambdal.com>
2025-01-02 17:08:04 -0500

    Add README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+Lambda Cloud CLI in Dart
+=====
+
+A prototype.
+
+Building
+----
+
+Here’s a puzzle: `//lib/src/secrets.dart` is missing. Can you figure out its format and contents?
+
+Once you do, `make`. The result will be in `//bin`.
--- bin/lambda_cli.dart
+++ bin/lambda_cli.dart
@@ -2,7 +2,7 @@ import 'package:args/command_runner.dart';
 import 'package:lambda_cli/src/commands/filesystems.dart';
 import 'package:lambda_cli/src/commands/instance_types.dart';
 import 'package:lambda_cli/src/commands/instances.dart';
-import 'package:lambda_cli/secrets.dart';
+import 'package:lambda_cli/src/secrets.dart';
 import 'package:openapi/api.dart';
 
 void main(List<String> arguments) {
